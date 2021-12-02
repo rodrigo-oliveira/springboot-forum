9246df767ab6201fe8a2e6f8c282c2eae843e640
Rodrigo Oliveira <rodrigo_oliveira@live.com>
2021-12-02 01:14:58 -0300

    Adicionado data-jpa e h2database

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -3,6 +3,7 @@ package com.example.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import com.example.forum.controller.dto.TopicoDto;
 import com.example.forum.model.Curso;
 import com.example.forum.model.Topico;
 
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TopicosController {
     @RequestMapping("/topicos")
-    public List<Topico> lista() {
-        Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+    public List<TopicoDto> lista() {
+        Topico topico = new Topico(
+            "Duvida", "Duvida com Spring", new Curso("Spring", "Programação")
+        );
 
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 }
